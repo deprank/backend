cb4944fe1fdb50a240908b826e9fd54a27da66a9
Eguo Wang <wangeguo@gmail.com>
2025-06-01 16:45:57 +0800

    refactor: move original analyer.rs to analyzers/rust.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod analyzers;
 pub mod app;
 pub mod config;
 pub mod context;
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod analyzer;
 pub mod contracts;
 pub mod storage;
 pub mod workflow;
